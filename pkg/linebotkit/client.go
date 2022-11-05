b0f828b3fd2715af44821a0da574a6ad42bf4b9f
amikai <as23041248@gmail.com>
2022-11-05 14:57:03 +0800

    impl linebot kit

---
+++ pkg/linebotkit/client.go
@@ -0,0 +1,33 @@
+package linebotkit
+
+import (
+	"context"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"go.uber.org/zap"
+
+	"github.com/amikai/line-go-prac/pkg/logkit"
+)
+
+type LinebotConfig struct {
+	ChannelSecret string
+	ChannelToken  string
+}
+
+type LinebotClient struct {
+	*linebot.Client
+}
+
+func NewClient(ctx context.Context, conf *LinebotConfig) *LinebotClient {
+	logger := logkit.FromContext(ctx)
+
+	bot, err := linebot.New(conf.ChannelSecret, conf.ChannelToken)
+	if err != nil {
+		logger.Fatal("failed to create Linebot client", zap.Error(err))
+	}
+
+	logger.Info("create Linebot client successfully")
+	return &LinebotClient{
+		Client: bot,
+	}
+}
